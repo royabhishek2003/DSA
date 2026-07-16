394f495a4cd0403ee93e2d59ff945f4ef29862a8
Abhishek Kumar Roy <145221033+royabhishek2003@users.noreply.github.com>
2026-07-17 00:55:53 +0530

    Time: 36 ms (37.08%) | Memory: 47.8 MB (20.94%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,42 +1,45 @@
 class Solution {
 public:
-    int leastInterval(vector<char>& tasks, int n) {
-        vector<int> freq(100);
-        for(char &ch: tasks){
-            freq[(int)ch]++;
+    int leastInterval(vector<char>& tasks, int p) {
+        int n = tasks.size();
+        unordered_map<char, int> mp;
+        
+        for(char &ch : tasks) {
+            mp[ch]++;
         }
-        priority_queue<pair<int,char>> pq;
-        for(int i=0;i<100;i++){
-            if(freq[i]!=0){
-                pq.push({freq[i],(char)i});
-            }
-        }
-        unordered_map<char,int> mp;
-        int day =1;
-        while(!pq.empty()){
-            queue<pair<int,int>> q;
-            while(!pq.empty() && mp.count(pq.top().second) && day <= mp[pq.top().second] + n){
-                int count=pq.top().first;
-                int character= pq.top().second;
-                pq.pop();
-                q.push({count,character});
-            }
-            if(!pq.empty()){
-                int count=pq.top().first;
-                int character= pq.top().second;
-                pq.pop();
-                count--;
-                mp[character] = day;
-                if(count) pq.push({count,character});  
-            } 
-            while(!q.empty()){
-                    pq.push({q.front().first, q.front().second});
-                    q.pop();
-            }         
-            day++;
-        }   
 
-        return day -1;
+        priority_queue<int> pq; //max heap
+        //we want to finish the process which is most occurring (having highest frequency)
+        //so that we don't have to finish in the last with p gaps.
+        int time = 0;
+        
+        for(auto &it : mp) {
+            pq.push(it.second);
+        }
+        
+        while(!pq.empty()) {
+            vector<int> temp;
+            for(int i = 1; i<=p+1; i++) {
+                //filling first p+1 characters
+                if(!pq.empty()) {
+                    temp.push_back(pq.top()-1); //finishing one instance of each process
+                    pq.pop();
+                }
+            }
+            
+            for(int &freq : temp) {
+                if(freq > 0)
+                    pq.push(freq);
+            }
+            
+            if(pq.empty()) //all processes finished
+                time += temp.size();
+            else
+                time += (p+1); //we finished p+1 tasks above in the loop
+            
+        }
+        
+        return time;
         
     }
 };
