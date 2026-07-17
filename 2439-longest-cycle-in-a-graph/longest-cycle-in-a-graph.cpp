2e87726cb8596a4265506e5d70721ced27a22de0
Abhishek Kumar Roy <145221033+royabhishek2003@users.noreply.github.com>
2026-07-18 02:36:47 +0530

    Time: 31 ms (61.30%) | Memory: 109 MB (59.59%) - LeetSync

---
+++ 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maxlengthcycle = -1;
+    void dfs(int u, auto &visited, auto &inrecursion,auto &count,auto &edges){
+        int v= edges[u];
+        visited[u]=true;
+        inrecursion[u]=true;
+        if(v == -1) {
+            inrecursion[u]=false;
+            return;
+        }
+        
+        if(visited[v] && inrecursion[v]){
+            maxlengthcycle= max(maxlengthcycle,count[u]-count[v]+1);
+        }
+        count[v]=count[u] + 1;
+        if(!visited[v])
+        dfs(v,visited,inrecursion,count,edges);
+        inrecursion[u]=false;
+    }
+    int longestCycle(vector<int>& edges) {
+        int n= edges.size();
+        vector<bool> visited(n,false);
+        vector<bool> inrecursion(n,false);
+        vector<int> count(n,1);
+
+        for(int i=0;i<n;i++){
+            if(edges[i]!=-1 && !visited[i]){
+                dfs(i,visited,inrecursion,count,edges);
+            }
+        }
+        return maxlengthcycle;
+    }
+};
