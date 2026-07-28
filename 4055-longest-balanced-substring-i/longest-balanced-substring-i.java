8c940cdb06a66399aa834675588f2df81e09a850
Abhishek Kumar Roy <145221033+royabhishek2003@users.noreply.github.com>
2026-07-28 23:31:47 +0530

    Time: 176 ms (29.38%) | Memory: 46.9 MB (43.53%) - LeetSync

---
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean allsamefreq(int []freq){
+        int count=0;
+        for(int i=0;i<26;i++){
+            if(count==0 && freq[i]!=0) count=freq[i];
+            if(count!=0 && count!=freq[i] && freq[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public int longestBalanced(String s) {
+        int n= s.length();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int freq[]= new int[26];
+            for(int j=i;j<n;j++){
+                freq[s.charAt(j)-'a']++;
+                if(allsamefreq(freq)) ans= Math.max(ans,j-i+1);
+            }
+        }
+        return ans;
+    }
+}
