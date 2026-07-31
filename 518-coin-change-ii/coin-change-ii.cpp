6e606fbac0de6854863743d256c9d5702a7ded7e
Abhishek Kumar Roy <145221033+royabhishek2003@users.noreply.github.com>
2026-07-31 13:50:56 +0530

    Time: 47 ms (24.13%) | Memory: 37.2 MB (20.63%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -33,6 +33,18 @@ public:
 
 
         vector<int> t(amount+1,0);
+        t[0]=1;
+        for(int &coin : coins){
+            for(int i=1;i<=amount;i++){
+                int take=0;
+                if(i>=coin){
+                    take= 1+ t[i];
+                }
+                int not_take= t[i-1];
+                t[i]= take+not_take;
+            }
+        }
+        return t[amount];
         
         
     }
