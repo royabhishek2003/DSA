5ba3dc3b1287d6c751190406a47eb7a2645930e0
Abhishek Kumar Roy <145221033+royabhishek2003@users.noreply.github.com>
2026-08-12 00:05:21 +0530

    Time: 79 ms (51.36%) | Memory: 101.8 MB (19.61%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
@@ -2,32 +2,32 @@ class Solution {
 public:
     int n;
     int dp[50001];
-    int solve(auto&jobs, int i){
+    int solve(auto& jobs, int i,auto& start){
         if(i>=n){
             return 0;
         }
         if(dp[i]!=-1) return dp[i];
-        int nxt = lower_bound(
-        jobs.begin(), jobs.end(), jobs[i][1],
-        [](const vector<int>& job, int start) {
-            return job[0] < start;
-        }
-    ) - jobs.begin();
+        int nxt = lower_bound(start.begin(),start.end(),jobs[i][1]) - start.begin();
 
-        int take= jobs[i][2] + solve(jobs,nxt);
-        int not_take= solve(jobs,i+1);
+        int take= jobs[i][2] + solve(jobs,nxt,start);
+        int not_take= solve(jobs,i+1,start);
 
         return dp[i]= max(take,not_take);
 
     }
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
         vector<vector<int>> jobs;
+        
         n= startTime.size();
         for(int i=0;i<n;i++){
             jobs.push_back({startTime[i],endTime[i],profit[i]});
         }
         sort(jobs.begin(),jobs.end());
+        vector<int>start;
+        for(vector<int> &vec: jobs){
+            start.push_back(vec[0]);
+        }
         memset(dp,-1,sizeof(dp));
-        return solve(jobs,0);
+        return solve(jobs,0,start);
     }
 };
