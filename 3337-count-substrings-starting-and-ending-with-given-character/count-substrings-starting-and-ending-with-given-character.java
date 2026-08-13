47134869bde85be53032d9931fe597d1b47b7a46
Abhishek Kumar Roy <145221033+royabhishek2003@users.noreply.github.com>
2026-08-13 23:49:34 +0530

    Time: 2 ms (36.86%) | Memory: 46.5 MB (39.70%) - LeetSync

--- 3337-count-substrings-starting-and-ending-with-given-character/count-substrings-starting-and-ending-with-given-character.java
+++ 3337-count-substrings-starting-and-ending-with-given-character/count-substrings-starting-and-ending-with-given-character.java
@@ -2,15 +2,23 @@ class Solution {
     public long countSubstrings(String s, char c) {
         // linear solution jab bhi naya c dekgo wo khud ek substring hai toh count+1 toh hoga aur pehle jitne c dekh chuke hinge utna aur count me add hoga kyuki uske sath bhi substring bana sakte hai 
 
-        long prevcount=0;
-        long count=0;
+        // long prevcount=0;
+        // long count=0;
+        // for(char ch: s.toCharArray()){
+        //     if(ch==c){
+        //         count+=1;
+        //         count+=prevcount;
+        //         prevcount+=1;
+        //     }
+        // }
+        // return count;
+
+        // 2nd approach is i have n (c) then it can be arranged in n(n+1)/2 ways 
+
+        int count=0;
         for(char ch: s.toCharArray()){
-            if(ch==c){
-                count+=1;
-                count+=prevcount;
-                prevcount+=1;
-            }
+            if(ch==c) count++;
         }
-        return count;
+        return (long)count*(count+1)/2;
     }
 }
