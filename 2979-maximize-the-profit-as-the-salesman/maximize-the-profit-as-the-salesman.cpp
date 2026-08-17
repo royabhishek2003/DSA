18f26586144c2d8c570bd13c02d2f521664e6dd2
Abhishek Kumar Roy <145221033+royabhishek2003@users.noreply.github.com>
2026-08-17 15:09:03 +0530

    Time: 98 ms (30.68%) | Memory: 97.7 MB (98.47%) - LeetSync

---
+++ 2979-maximize-the-profit-as-the-salesman/maximize-the-profit-as-the-salesman.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int size;
+    int dp[100001];
+    int solve(auto &offers, int i){
+        if(i>=size){
+            return 0;
+        }
+        if(dp[i]!=-1) return dp[i];
+        int next = upper_bound(
+        offers.begin() + i + 1,
+        offers.end(),
+        offers[i][1],
+        [](int end, const vector<int>& offer) {
+            return end < offer[0];
+        }
+    ) - offers.begin();
+    int take = offers[i][2] + solve(offers, next);
+        int not_take= solve(offers,i+1);
+        return dp[i]=max(take,not_take);
+        
+    }
+    int maximizeTheProfit(int n, vector<vector<int>>& offers) {
+        sort(offers.begin(),offers.end());
+        size= offers.size();
+        memset(dp,-1,sizeof(dp));
+
+        return solve(offers,0);
+    }
+};
