8dbe024b074bf0c24b69be91926700342a214467
Abhishek Kumar Roy <145221033+royabhishek2003@users.noreply.github.com>
2026-08-19 13:26:19 +0530

    Time: 19 ms (14.09%) | Memory: 17.6 MB (10.23%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if(numRows ==1) return s;
+        vector<vector<char>> temp(numRows);
+        int i=0;
+        bool reverseorder=false;
+        for(char &ch: s){
+            temp[i].push_back(ch);
+            if(reverseorder==false){
+                i++;
+                if(i==numRows){
+                    i=numRows-2;
+                    reverseorder=true;
+                }
+            }
+            else if(reverseorder==true){
+                i--;
+                if(i<0){
+                    i=1;
+                    reverseorder=false;
+                }
+            }
+        }
+
+        string result="";
+        for(vector<char> &vec: temp){
+            for(char &ch: vec) result +=ch;
+        }
+
+        return result;
+    }
+};
